498dd99e2e58a4db06cea150a7dda0a7f353d261
swaya <swaya@10.0.0.100>
2022-01-24 18:49:48 -0500

    level assets

---
+++ RobotGamee/src/animation/levels.java
@@ -0,0 +1,21 @@
+package animation;
+
+public class levels {
+
+	private static AnimationMain[] level = new AnimationMain[5];
+
+	public static void i() {
+		// level 1
+//		level[0] = new AnimationMain(5);
+		// level 2
+//		level[1] = new AnimationMain(10);
+		// level 3
+//		level[2] = new AnimationMain(15);
+		// level 4
+//		level[3] = new AnimationMain(20);
+		// level 5
+//		level[4] = new AnimationMain(5);
+
+	}
+
+}

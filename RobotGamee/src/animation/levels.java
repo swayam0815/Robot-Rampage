73ea4ce38d7fffdbee642701c96fac18879cf3de
Hamir <Hamir@192.168.0.29>
2022-01-25 20:01:31 -0500

    figuring out button placements for levels

--- RobotGamee/src/animation/levels.java
+++ RobotGamee/src/animation/levels.java
@@ -11,19 +11,38 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import hsa2.*;
 
 public class levels {
-
+	
+	public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException, InterruptedException {
+		new setInitialValues(gc, GRWIDTH, GRHEIGHT);
+		new levels(gc);
+	}
 	private static AnimationMain[] level = new AnimationMain[5];
 	private static Dimension GRsize = Toolkit.getDefaultToolkit().getScreenSize(); // creates a variable to get screen
 
 	private static int GRHEIGHT = (int) GRsize.getHeight() - 70; // (int)GRsize.getHeight() - 70
 	private static int GRWIDTH = (int) (GRHEIGHT * 1.777777777778); // this sets the size of the grid to fit
+
+	private static GraphicsConsole gc = new GraphicsConsole(GRWIDTH, GRHEIGHT);
+
+	//rectangles
+	private Rectangle lvl1 = new Rectangle((int)(GRWIDTH / 5.923), (int)(GRHEIGHT / 3.421), (int)(GRWIDTH / 9.625), (int)(GRWIDTH / 9.625));
+	private Rectangle lvl2 = new Rectangle(835, (int)(GRHEIGHT / 3.421), (int)(GRWIDTH / 9.625), (int)(GRWIDTH / 9.625));
+	private Rectangle lvl3 = new Rectangle((int)(GRWIDTH / 5.923), 370, (int)(GRWIDTH / 9.625), (int)(GRWIDTH / 9.625));
+	private Rectangle lvl4 = new Rectangle(835, 370, (int)(GRWIDTH / 9.625), (int)(GRWIDTH / 9.625));
+	private Rectangle lvlBoss = new Rectangle(460, (int)(GRHEIGHT / 3.421), 233, 270);
+	
+	//images
 	private static Image lvl;
 	private static Image cursorImg;
 	private static Image cursorClicked;
-
+	private static Image bossImg;
+	private static Image bossLight;
+	private static Image bossDark;
+	
+	
 	// the screen
 
-	private static GraphicsConsole gc;
+//	private static GraphicsConsole gc;
 	// = new GraphicsConsole(GRWIDTH, GRHEIGHT);
 	private static Rectangle cursor = new Rectangle(GRWIDTH / 2, GRHEIGHT / 2, GRHEIGHT / 100, GRHEIGHT / 100); // to
 
@@ -46,7 +65,10 @@ public class levels {
 	}
 
 	private static void init() throws IOException {
-		lvl = ImageIO.read(new File("levelBlank.png"));
+		gc.enableMouse();
+		gc.enableMouseMotion();
+		
+		lvl = ImageIO.read(new File("levelMenu.png"));
 		cursorImg = ImageIO.read(new File("cursor.png"));
 		cursorClicked = ImageIO.read(new File("cursor clicked.png"));
 
@@ -59,6 +81,13 @@ public class levels {
 			// background
 			gc.drawImage(lvl, 0, 0, GRWIDTH, GRHEIGHT);
 
+			gc.setColor(Color.GREEN);
+			gc.drawRect(lvl1);
+			gc.drawRect(lvl2);
+			gc.drawRect(lvl3);
+			gc.drawRect(lvl4);
+			gc.drawRect(lvlBoss);
+			
 			// cursor
 			if (gc.getMouseButton(0))
 				gc.drawImage(cursorClicked, cursor.x, cursor.y - cursor.width * 2, cursor.width * 15,
